484ff3ecc3ceb89c3ebd8457ef84530a88a4a601
Keith Kwon <keithkwon.dev@gmail.com>
2021-03-15 10:53:23 +0900

    반복제어문3

--- 147.py
+++ 147.py
@@ -1,9 +1,12 @@
 n = int(input())
 
-count = 0
+count = 1
+
 
 for i in range(n):
     for j in range(i):
-        print(' ', end='')
-    for j in range(n- i):
-        print(count, end = ' ')
+        print(' ', end=' ')
+    for j in range(n-i):
+        print(count, end=' ')
+        count += 1
+    print()
